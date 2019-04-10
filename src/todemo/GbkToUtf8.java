f8398c86e6e2f7fdf0d6ec23ee47472c41bfa9ad
Juch <Juch@live.cn>
2019-04-10 16:33:25 +0800

    优化log面板
    Signed-off-by: Juch <Juch@live.cn>

--- src/todemo/GbkToUtf8.java
+++ src/todemo/GbkToUtf8.java
@@ -15,11 +15,13 @@ import static todemo.ToUTF8.toUTF8;
 class GbkToUtf8 {
 
     static ArrayList<String> fileList = new ArrayList<String>();
+    static StringBuilder sb=new StringBuilder();
 
     private GbkToUtf8(String fileName) throws IOException {
         String keyPre ="GB2312";
         if (keyPre .equals(getEncodingByPath(fileName))) {
-            fileArea.setText(fileName+"\n");
+//            fileArea.setText(fileName+"\n");
+            sb.append(fileName).append("\n");
             String outFile = toUTF8(fileName);
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "utf-8"));
             out.write(outFile);
@@ -36,6 +38,8 @@ class GbkToUtf8 {
                 new GbkToUtf8(fileName);
             }
         }
-        fileArea.setText("编码转换完成切勿重复操作!!"+"\n");
+//        fileArea.setText("编码转换完成切勿重复操作!!"+"\n");
+        sb.append("编码转换完成切勿重复操作!!");
+        fileArea.setText(sb.toString());
     }
 }
